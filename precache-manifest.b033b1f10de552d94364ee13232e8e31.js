554b2bf99e19fc907f884280c20da03ca8b0fb69
coollucky61 <laxmichand.chijwani@gmail.com>
2022-02-27 12:39:08 +0530

    Updates

---
+++ precache-manifest.b033b1f10de552d94364ee13232e8e31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1851e29fabce1d88a4bae6f7f71f22db",
+    "url": "%20.github.io/Image-Compressor-REACT-/index.html"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "%20.github.io/Image-Compressor-REACT-/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "%20.github.io/Image-Compressor-REACT-/static/js/2.69fef3bc.chunk.js"
+  },
+  {
+    "revision": "fe2a0a87bfbe8ec285e5",
+    "url": "%20.github.io/Image-Compressor-REACT-/static/js/main.8ad86672.chunk.js"
+  },
+  {
+    "revision": "fd4df4257dcdd87e00d9",
+    "url": "%20.github.io/Image-Compressor-REACT-/static/js/runtime-main.d8d67bed.js"
+  }
+]);
